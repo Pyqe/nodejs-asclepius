feffc93514d1979c5c46df307b5cb41a83e597f8
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2024-06-07 04:09:31 +0700

    Add cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,10 @@ const crypto = require('crypto');
     
     const server = Hapi.server({
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        port: 3000
+        port: 3000,
+        routes: {
+            cors: true
+        }
     });
 
 
